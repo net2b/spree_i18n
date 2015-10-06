e452940e83f350fab5a2d2b8de644e5abcba1348
Elia Schito <elia@schito.me>
2015-10-06 11:18:14 +0200

    Fix the migration to use change

--- db/migrate/20151005110021_migrate_all_translate_columns_to_h_store.rb
+++ db/migrate/20151005110021_migrate_all_translate_columns_to_h_store.rb
@@ -1,23 +1,22 @@
 require 'trasto'
 
 class MigrateAllTranslateColumnsToHStore < ActiveRecord::Migration
-  def up
-    {
-      spree_option_types:        [:name, :presentation],
-      spree_option_values:       [:name, :presentation],
-      spree_products:            [:name, :description, :meta_description, :meta_keywords, :slug],
-      spree_product_properties:  [:value],
-      spree_promotions:          [:name, :presentation],
-      spree_properties:          [:name, :presentation],
-      spree_taxons:              [:name, :description, :meta_title, :meta_description, :meta_keywords, :permalink],
-      spree_taxonomies:          [:name],
-    }.each do |table, columns|
+  TABLES_AND_COLUMNS = {
+    spree_option_types:        [:name, :presentation],
+    spree_option_values:       [:name, :presentation],
+    spree_products:            [:name, :description, :meta_description, :meta_keywords, :slug],
+    spree_product_properties:  [:value],
+    spree_promotions:          [:name, :description],
+    spree_properties:          [:name, :presentation],
+    spree_taxons:              [:name, :description, :meta_title, :meta_description, :meta_keywords, :permalink],
+    spree_taxonomies:          [:name],
+  }
+
+  def change
+    type = Trasto.supports_hstore? ? :hstore : :text
+    TABLES_AND_COLUMNS.each do |table, columns|
       columns.each do |column|
-        if Trasto.supports_hstore?
-          add_column table, "#{column}_i18n", :hstore
-        else
-          add_column table, "#{column}_i18n", :text, default: nil
-        end
+        add_column table, "#{column}_i18n", :hstore
       end
     end
   end

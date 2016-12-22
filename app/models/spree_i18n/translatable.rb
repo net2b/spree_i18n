affcf188e71198828ea504735579e777f3e8bfd3
Elia Schito <elia@schito.me>
2016-12-22 14:32:09 +0100

    Fix translatable forms

--- app/models/spree_i18n/translatable.rb
+++ app/models/spree_i18n/translatable.rb
@@ -3,7 +3,12 @@ module SpreeI18n
     extend ActiveSupport::Concern
 
     def translations_attributes=values
-      raise values
+      values.each do |position, data|
+        locale = data["locale"].parameterize.downcase.underscore
+        data.except("locale").each do |attribute, value|
+          send("#{attribute}_#{locale}=", value)
+        end
+      end
     end
   end
 end
--- config/initializers/form_builder_globalize_patch.rb
+++ config/initializers/form_builder_globalize_patch.rb
@@ -44,7 +44,6 @@ module ActionView
         @index      = @index ? @index + 1 : 1
         object_name = "#{@object_name}[translations_attributes][#{@index}]"
         object      = @object.send(:translation_for, locale)
-        @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : "")
         @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale)
         @template.fields_for(object_name, object, *args, &proc)
       end
